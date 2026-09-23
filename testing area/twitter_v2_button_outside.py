7160b9b0aab2f3731a77b68831813ca8e986118d
uesu <39020293+uesu@users.noreply.github.com>
2026-09-23 18:38:15 +0800

    Update twitter_v2_button_outside.py

--- testing area/twitter_v2_button_outside.py
+++ testing area/twitter_v2_button_outside.py
@@ -212,7 +212,7 @@ STATIC_BUTTONS = [
         "emoji": {"id": "1439878792653832253", "name": "starward11", "animated": True},
     },
     {
-        "label": "Support",
+        "label": "Donate",
         "url": "https://ko-fi.com/jieunlatte",
         "emoji": {"id": "1509026327548657914", "name": "starwardfans", "animated": True},
     },
