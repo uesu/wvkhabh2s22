65a430afd1d6989b4d1885b32bd288ccccd051df
uesu <39020293+uesu@users.noreply.github.com>
2026-09-23 18:36:19 +0800

    Update reddit_main.py

--- testing area/reddit_main.py
+++ testing area/reddit_main.py
@@ -143,7 +143,7 @@ MIRROR_HOST = _raw_mirror if _raw_mirror else "redditez.com"
 # ---------------------------------------------------------------------------
 STATIC_BUTTONS = [
     {"label": "Citlali News", "url": "https://discord.gg/HyrVP9wRXu", "emoji": {"id": "1439878792653832253", "name": "starward11", "animated": True}},
-    {"label": "Support", "url": "https://ko-fi.com/jieunlatte", "emoji": {"id": "1509026327548657914", "name": "starwardfans", "animated": True}},
+    {"label": "Donate", "url": "https://ko-fi.com/jieunlatte", "emoji": {"id": "1509026327548657914", "name": "starwardfans", "animated": True}},
 ]
 
 # Matches watch / shorts / youtu.be links inside the RSS entry HTML
